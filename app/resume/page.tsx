567a96720d26121eefddfe3c9f4201d1b8105895
natnaelengeda <nattynengeda@gmail.com>
2024-01-04 21:36:55 +0300

    Update Resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -20,7 +20,7 @@ export default function Resume() {
       }
       <iframe
         className='w-full h-[85vh]'
-        src="https://drive.google.com/file/d/1wAUVzDrHJ6nQ0nZ0z7EzfmOTyhGgcp4H/preview"
+        src="https://drive.google.com/file/d/1I31guSUpkxRqD_1EcHh9faX_QAjCqsZb/preview"
         allow="autoplay"
         onLoad={() => setLoading(!Loading)}
       />
