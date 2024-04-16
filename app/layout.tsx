aded1c996e67ba7d45915ba7b6f922a8eb6ce8ed
nattyengeda <nattynengeda@gmail.com>
2024-04-16 12:49:37 +0300

    Update SEO image URLs in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
     siteName: "Natnael Engeda - Portfolio",
     images: [
       {
-        url: "/seo-image.jpg",
+        url: "https://i.ibb.co/nQQ2rd0/seo-image.jpg",
         width: 1200,
         height: 630,
       }
@@ -71,7 +71,7 @@ export const metadata: Metadata = {
     title: 'Natnael Engeda',
     description: "Natnael Engeda - Software Developer | Fullstack Developer | Mobile App Developer | Reactjs | Nodejs | Flutter ",
     site: "@NatnaelEngeda",
-    images: ['/seo-image.jpg'],
+    images: ['https://i.ibb.co/nQQ2rd0/seo-image.jpg'],
     creator: "@NatnaelEngeda",
   },
   verification: {
